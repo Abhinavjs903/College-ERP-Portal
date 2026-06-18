3c1492c59c0b05dc13c5458f400a6b1ed3c69043
Abhinavjs903 <abhinavmait300@gmailcom>
2026-06-19 00:02:45 +0530

    Add navbar and hero styling

---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "../../styles/Hero.css";
+
+
+function Hero() {
+  return (
+    <section>
+      <h1>Open Source College ERP Platform</h1>
+
+      <p>
+        Manage students, teachers, attendance,
+        notices and administration from one place.
+      </p>
+
+      <button>Get Started</button>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../../styles/Navbar.css";
+
+
+function Navbar() {
+  return (
+    <nav>
+      <h2>College ERP Lite</h2>
+
+      <div>
+        <a href="#">Home</a>
+        <a href="#">Features</a>
+        <a href="#">Login</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
